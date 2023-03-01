1309ba7ced8ddafdb2ab680cd40608638dae1b8e
Sergey Bakulin <sergey.bakulin.github@gmail.com>
2023-03-01 23:57:29 +0600

    feat: add the `Multipart` class

---
+++ index.ts
@@ -0,0 +1,115 @@
+import { File as NodeFile } from "node:buffer";
+import { BindenError, Middleware, IMiddlewareParams, Context } from "binden";
+import busboy, { BusboyConfig } from "busboy";
+
+export type IMultipartConfig = Omit<BusboyConfig, "headers">;
+
+export interface IMultipartOptions extends IMiddlewareParams {
+  config?: IMultipartConfig;
+  throw_limits?: boolean;
+}
+
+export class Multipart extends Middleware {
+  readonly #config: IMultipartConfig;
+  readonly #throw_limits: boolean;
+
+  public constructor({
+    config = {},
+    throw_limits = true,
+    ...rest
+  }: IMultipartOptions = {}) {
+    super(rest);
+    this.#config = config;
+    this.#throw_limits = throw_limits;
+  }
+
+  public async run(context: Context): Promise<void> {
+    const { log, request } = context;
+    const { headers, method, content_type } = request;
+
+    switch (method) {
+      case "GET":
+      case "HEAD":
+      case "OPTIONS":
+      case "TRACE":
+        context.log.trace("Method does not support incoming body", { method });
+        return;
+      default:
+        break;
+    }
+
+    if (
+      typeof content_type?.boundary === "undefined" ||
+      content_type.boundary === null
+    ) {
+      log.trace("`Content-Type` is not `multipart/form-data`", {
+        "Content-Type": content_type?.type,
+      });
+      return;
+    }
+
+    await new Promise<void>((resolve, reject) => {
+      const fd = new FormData();
+      const bb = busboy({ headers, ...this.#config })
+        .once("error", (cause) => {
+          const message = "`busboy` internal error";
+          reject(new BindenError(415, { message, cause }));
+        })
+        .once("close", () => {
+          request.body = fd;
+          resolve();
+        })
+        .on("field", (name, value) => {
+          fd.append(name, value);
+        })
+        .on("file", (name, stream, { mimeType: type, filename }) => {
+          const chunks: Buffer[] = [];
+          stream
+            .once("close", () => {
+              fd.append(
+                name,
+                new NodeFile(chunks, filename, { type }) as File & NodeFile
+              );
+            })
+            .on("data", (chunk: Buffer) => {
+              chunks.push(chunk);
+            })
+            .once("limit", () => {
+              if (this.#throw_limits) {
+                const cause = new Error("`busboy` reached `fileSize` limit ");
+                const error = new BindenError(415, { cause });
+                reject(error);
+              }
+            });
+        })
+        .on("fieldsLimit", () => {
+          if (this.#throw_limits) {
+            const cause = new Error("`busboy` reached fields limit ");
+            const error = new BindenError(415, { cause });
+            reject(error);
+          }
+        })
+        .on("filesLimit", () => {
+          if (this.#throw_limits) {
+            const cause = new Error("`busboy` reached files limit ");
+            const error = new BindenError(415, { cause });
+            reject(error);
+          }
+        })
+        .once("partsLimit", () => {
+          if (this.#throw_limits) {
+            const cause = new Error("`busboy` reached parts limit ");
+            const error = new BindenError(415, { cause });
+            reject(error);
+          } else {
+            request.body = fd;
+            resolve();
+          }
+        });
+      request.pipe(bb);
+    });
+  }
+}
+
+export default (config: IMultipartConfig = {}): Multipart =>
+  new Multipart({ config });
